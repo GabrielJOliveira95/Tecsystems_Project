7ada3eb861397cc8c8551ed4016a7e3daaa9e7b3
Gabriel Oliveira <gabrieljoliveira95@gmail.com>
2019-10-26 18:47:13 -0300

    deleted codes unsed

--- src/Intership.java
+++ src/Intership.java
@@ -1,31 +1,15 @@
 
-import com.opencsv.CSVReader;
-import com.opencsv.exceptions.CsvException;
-import com.opencsv.exceptions.CsvValidationException;
-import java.io.BufferedReader;
+
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
 import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**
- *
- * @author ThinkPad Corporativo
- */
 public class Intership extends javax.swing.JFrame {
 
-    /**
-     * Creates new form Intership
-     */
+  
     public Intership() {
         initComponents();
     }
